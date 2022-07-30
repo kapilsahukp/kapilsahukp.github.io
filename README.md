043d95c4394f425ec28e3a38d46ad60e544e8687
Kapil Sahu <65108307+kapilsahukp@users.noreply.github.com>
2022-07-30 16:30:50 -0400

    Initial draft.

---
+++ README.md
@@ -0,0 +1,47 @@
+<h1 align="center">
+  💻<br>Portfolio - Kapil Sahu
+</h1>
+
+![Project Final Result](assets/image/preview.png)
+
+<h4 align="center"><a href="https://www.kapilsahukp.github.io/">Click to visit project</a></h4>
+
+## 📚 Sections
+
+The website consists of five sections:
+
+- **Home:** In it we have a brief presentation;
+- **About me:** In this section I have a description telling a little about who I am;
+- **Projects:** It presents some developed projects with a direct link to the respective codes on GitHub;
+- **Professional Experience:** Displays through cards, my previous and current work experience;
+- **Skills:** Displays through cards, my skills;
+- **Contact:** My email id and linkedin profile;
+
+---
+
+## 💼 Technologies used
+
+For the development of this site I used the following technologies:
+
+- HTML;
+- Sass;
+- JavaScript;
+- ScrollReveal;
+- SVG Inject;
+- Typed.js;
+
+---
+
+<h2>Author</h2>
+
+<table>
+  <tr>
+    <td align="center">
+      <a href="https://github.com/kapilsahukp">
+        <sub>
+          <b>Kapil Sahu</b>
+        </sub>
+      </a>
+    </td>
+  </tr>
+</table>
---
+++ assets/js/main.js
@@ -0,0 +1,65 @@
+import { initScrollReveal } from "./scrollReveal.js";
+import { typeWrite } from "./typeWrite.js";
+import { hoverChangeDescription } from "./hoverChangeDescription.js";
+import { menu } from "./menu.js";
+
+menu();
+initScrollReveal();
+typeWrite(document.querySelector(".typewriter"));
+
+hoverChangeDescription(
+  ".python",
+  "An experience of 4 years with Python programming language along with the environments like VS-Code, PyCharm, Spyder and Jupyter Lab."
+);
+hoverChangeDescription(
+  ".sql",
+  "An experience of 5 years with SQL programming language on tools like DataGrip, MS-SQL Server, SQLite and Oracle-SQL."
+);
+hoverChangeDescription(
+  ".tab",
+  "An experience of 1 year with creating insightful dashboards using Tableau."
+);
+hoverChangeDescription(
+  ".pandas",
+  "Using Pandas to manipulate the tabular data in an efficient way."
+);
+hoverChangeDescription(
+  ".numpy",
+  "Using NumPy to work with multi-dimensional arrays and matrices and perform scientific computation."
+);
+hoverChangeDescription(
+  ".qgis",
+  "Quantum Geographic Information System: To visualize, edit, process and analyze Geospatial Data."
+);
+hoverChangeDescription(
+  ".git",
+  "A version control system to keep track of the changes in codebase in a Software Development Lifecycle."
+);
+hoverChangeDescription(
+  ".r",
+  "An experience of 1 year of working with R using RStudio."
+);
+hoverChangeDescription(
+  ".run",
+  "Participated in the yearly Pune International Marathon."
+);
+hoverChangeDescription(
+  ".green",
+  "Conducted environment cleanliness awareness drives across the city to educate underprivileged students about the importance of clean surroundings and steps needed to make a difference around us."
+);
+hoverChangeDescription(
+  ".cook",
+  "I enjoy cooking and love to try new cuisines."
+);
+hoverChangeDescription(
+  ".med",
+  "To keep it all together!!"
+);
+hoverChangeDescription(
+  ".mail",
+  "To keep it all together!!"
+);
+hoverChangeDescription(
+  ".in",
+  "To keep it all together!!"
+);
